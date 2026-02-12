1798b160ec857e862f9034b8ebff8c0e978125b2
NatBaracho <natan.baracho@hotmail.com>
2026-02-11 21:13:10 -0300

    verificação_de_maior_idade

--- listaLogica-Nata/Program.cs
+++ listaLogica-Nata/Program.cs
@@ -3,51 +3,70 @@
 //Lista de Exercíco Logi
 //Nivel 1: Varíaveis e Operações Básicas
 #region 1. Calculadora de Média Simples
-Console.WriteLine("--- Cálculo de Média ---");
+using System.ComponentModel.Design;
 
-Console.Write("Digite a primeira nota: ");
-double nota1 = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("--- Cálculo de Média ---");
 
-Console.Write("Digite a segunda nota: ");
-double nota2 = Convert.ToDouble(Console.ReadLine());
+//Console.Write("Digite a primeira nota: ");
+//double nota1 = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Digite a terceira nota: ");
-double nota3 = Convert.ToDouble(Console.ReadLine());
+//Console.Write("Digite a segunda nota: ");
+//double nota2 = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Digite a quarta nota: ");
-double nota4 = Convert.ToDouble(Console.ReadLine());
+//Console.Write("Digite a terceira nota: ");
+//double nota3 = Convert.ToDouble(Console.ReadLine());
 
-double media = (nota1 + nota2 + nota3 + nota4) / 4;
+//Console.Write("Digite a quarta nota: ");
+//double nota4 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine($"Sua média é: {media:F1}");
-Console.ReadKey();
+//double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+//Console.WriteLine($"Sua média é: {media:F1}");
+//Console.ReadKey();
 #endregion
 
 // Converter Idade
 #region 2. Conversor de Idade
-Console.Write("Digite sua ideda: ");
-int idade = Convert.ToInt16(Console.ReadLine());
+//Console.Write("Digite sua ideda: ");
+//int idade = Convert.ToInt16(Console.ReadLine());
 
-int dias = (idade * 365);
-Console.WriteLine($"Seu dia de nascido é: {dias}");
-Console.ReadKey();
+//int dias = (idade * 365);
+//Console.WriteLine($"Seu dia de nascido é: {dias}");
+//Console.ReadKey();
 
 #endregion
 
 // Troca de Valore
 
 #region 3. Troca de valores
-double variavelA = 5689;
-double variavelB = 1023;
+//double variavelA = 5689;
+//double variavelB = 1023;
 
-Console.WriteLine($"Antes da troca -> variavelB: {variavelB} variavelA: {variavelA}\n");
+//Console.WriteLine($"Antes da troca -> variavelB: {variavelB} variavelA: {variavelA}\n");
 
-// Lógica de troca (Swap)
-double variavelC = variavelA; 
-variavelA = variavelB;        
-variavelB = variavelC;       
+//// Lógica de troca (Swap)
+//double variavelC = variavelA; 
+//variavelA = variavelB;        
+//variavelB = variavelC;       
 
-Console.WriteLine($"Depois da troca -> variavelB: {variavelB} variavelA: {variavelA}");
+//Console.WriteLine($"Depois da troca -> variavelB: {variavelB} variavelA: {variavelA}");
 
+//Console.ReadKey();
+#endregion
+
+
+//Nivel 2: Estruturas Condicionais (if, else if, else)
+#region 1: Verificador de Maioridade
+Console.Write("Digite sua ideda: ");
+int idade1 = Convert.ToInt16(Console.ReadLine());
+
+if (idade1 >= 18)
+{
+    Console.WriteLine("Entrado autorisada");
+}
+else
+{
+    Console.WriteLine("Entrada não autorisada");
+}
 Console.ReadKey();
 #endregion
