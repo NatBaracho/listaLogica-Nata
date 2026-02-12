3d6cac57ef1a654b0e784364f3828fb05393ca63
NatBaracho <natan.baracho@hotmail.com>
2026-02-11 21:04:23 -0300

    troca_de_valores

--- listaLogica-Nata/Program.cs
+++ listaLogica-Nata/Program.cs
@@ -3,24 +3,24 @@
 //Lista de Exercíco Logi
 //Nivel 1: Varíaveis e Operações Básicas
 #region 1. Calculadora de Média Simples
-//Console.WriteLine("--- Cálculo de Média ---");
+Console.WriteLine("--- Cálculo de Média ---");
 
-//Console.Write("Digite a primeira nota: ");
-//double nota1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite a primeira nota: ");
+double nota1 = Convert.ToDouble(Console.ReadLine());
 
-//Console.Write("Digite a segunda nota: ");
-//double nota2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite a segunda nota: ");
+double nota2 = Convert.ToDouble(Console.ReadLine());
 
-//Console.Write("Digite a terceira nota: ");
-//double nota3 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite a terceira nota: ");
+double nota3 = Convert.ToDouble(Console.ReadLine());
 
-//Console.Write("Digite a quarta nota: ");
-//double nota4 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite a quarta nota: ");
+double nota4 = Convert.ToDouble(Console.ReadLine());
 
-//double media = (nota1 + nota2 + nota3 + nota4)/4;
+double media = (nota1 + nota2 + nota3 + nota4) / 4;
 
-//Console.WriteLine($"Sua média é: {media:F1}");
-//Console.ReadKey();
+Console.WriteLine($"Sua média é: {media:F1}");
+Console.ReadKey();
 #endregion
 
 // Converter Idade
@@ -32,4 +32,22 @@ int dias = (idade * 365);
 Console.WriteLine($"Seu dia de nascido é: {dias}");
 Console.ReadKey();
 
+#endregion
+
+// Troca de Valore
+
+#region 3. Troca de valores
+double variavelA = 5689;
+double variavelB = 1023;
+
+Console.WriteLine($"Antes da troca -> variavelB: {variavelB} variavelA: {variavelA}\n");
+
+// Lógica de troca (Swap)
+double variavelC = variavelA; 
+variavelA = variavelB;        
+variavelB = variavelC;       
+
+Console.WriteLine($"Depois da troca -> variavelB: {variavelB} variavelA: {variavelA}");
+
+Console.ReadKey();
 #endregion
